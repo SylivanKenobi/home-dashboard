60818b39379e46aeed50c7f72010af374e07e62b
Sylvain <sypsilon@hotmail.com>
2020-07-21 16:51:53 +0200

    Add Slideshow

---
+++ app/jobs/slideshow.rb
@@ -0,0 +1,17 @@
+require 'net/http'
+require 'json'
+require 'pry'
+
+require_relative '../../lib/secrets'
+
+id = "slideshow"
+
+
+SCHEDULER.every '15s', :first_in => 0 do
+  send_event(id, image: random_image)
+end
+
+def random_image
+  images = Dir["assets/images/slideshow/*"]
+  images[rand(images.length)].gsub(/\/images/,"")
+end
