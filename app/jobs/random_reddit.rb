58e8c5d2e1f0a8e8ecc3462a7496387c58dd98eb
Sylvain Gilgen <sypsilon@hotmail.com>
2021-01-16 21:35:22 +0100

    Add subreddit

--- app/jobs/random_reddit.rb
+++ app/jobs/random_reddit.rb
@@ -1,29 +1,21 @@
 require 'net/http'
 require 'json'
 
-
-id = "aww"
-
 SCHEDULER.every '15m', :first_in => 0 do
-   send_event(id, image: get_post)
+   send_event("picture", image: get_post({itookapicture: 75}))
+   send_event("bike", image: get_post({Bikeporn: 150, mountainbiking: 150, FixedGearBicycle: 100}))
 end
 
-def get_post
-    subreddit = 'Bikeporn'
-    placeholder = '/assets/sunset.png'
-    url = "https://www.reddit.com/r/#{subreddit}/hot.json?limit=100"
+def get_post(subreddits)
+    urls = []
 
-    response = RestClient::Request.execute(
-        method: :get, url: url
-      )
-    json = JSON.parse(response.body)
-    if json['data']['children'].count <= 0
-      return placeholder
-    else
-      urls = json['data']['children'].map{|child| child['data']['url'] }
-
-      # Ensure we're linking directly to an image, not a gallery etc.
-      valid_urls = urls.select{|url| url.downcase.end_with?('png', 'gif', 'jpg', 'jpeg')}
-      return "background-image:url(#{valid_urls.sample(1).first})"
+    subreddits.each do |subreddit, posts|
+      url = "https://www.reddit.com/r/#{subreddit}/hot.json?limit=#{posts}"
+      response = RestClient::Request.execute( method: :get, url: url )
+      json = JSON.parse(response.body)
+      urls = urls + json['data']['children'].map{ |child| child['data']['url'] }
     end
+
+    valid_urls = urls.select{|url| url.downcase.end_with?('png', 'gif', 'jpg', 'jpeg')}
+    return "background-image:url(#{valid_urls.sample})"
 end
